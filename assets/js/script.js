f97c55cdc0eb9e1ff8a72a0b55df5188bd072f3e
Andrew Baxter <asbaxter1@gmail.com>
2022-06-28 15:27:46 -0400

    got the whole program working finally, code is very ugly so now time to simplify the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,49 +13,127 @@ function saveTask(){
 };
 
 function time(){
-    
+    let timeSlot = 0;
     let hour = moment().hour();
 
     if (hour === 9){
         let currentTime = document.getElementById("9");
         currentTime.classList.add("present");
+        timeSlot = 9;
     }
-    else if (hour === 10){
+    else if (timeSlot < hour){
+        let currentTime = document.getElementById("9");
+        currentTime.classList.add("past");
+    }
+    else {
+        let currentTime = document.getElementById("9");
+        currentTime.classList.add("future");
+    };
+    if (hour === 10){
         let currentTime = document.getElementById("10");
         currentTime.classList.add("present");
+        timeSlot = 10;
     }
-    else if (hour === 11){
+    else if (timeSlot < hour){
+        let currentTime = document.getElementById("10");
+        currentTime.classList.add("past");
+    }
+    else {
+        let currentTime = document.getElementById("10");
+        currentTime.classList.add("future");
+    };
+    if (hour === 11){
         let currentTime = document.getElementById("11");
         currentTime.classList.add("present");
+        timeSlot = 11;
     }
-    else if (hour === 12){
+    else if (timeSlot < hour){
+        let currentTime = document.getElementById("11");
+        currentTime.classList.add("past");
+    }
+    else {
+        let currentTime = document.getElementById("11");
+        currentTime.classList.add("future");
+    };
+    if (hour === 12){
         let currentTime = document.getElementById("12");
         currentTime.classList.add("present");
+        timeSlot = 9;
     }
-    else if (hour === 13){
+    else if (timeSlot < hour){
+        let currentTime = document.getElementById("12");
+        currentTime.classList.add("past");
+    }
+    else {
+        let currentTime = document.getElementById("12");
+        currentTime.classList.add("future");
+    };
+    if (hour === 13){
         let currentTime = document.getElementById("13");
         currentTime.classList.add("present");
+        timeSlot = 13;
     }
-    else if (hour === 14){
+    else if (timeSlot < hour){
+        let currentTime = document.getElementById("13");
+        currentTime.classList.add("past");
+    }
+    else {
+        let currentTime = document.getElementById("13");
+        currentTime.classList.add("future");
+    };
+    if (hour === 14){
         let currentTime = document.getElementById("14");
         currentTime.classList.add("present");
+        timeSlot = 14;
     }
-    else if (hour === 15){
+    else if (timeSlot < hour){
+        let currentTime = document.getElementById("14");
+        currentTime.classList.add("past");
+    }
+    else {
+        let currentTime = document.getElementById("14");
+        currentTime.classList.add("future");
+    };
+    if (hour === 15){
         let currentTime = document.getElementById("15");
         currentTime.classList.add("present");
+        timeSlot = 15;
     }
-    else if (hour === 16){
+    else if (timeSlot < hour){
+        let currentTime = document.getElementById("15");
+        currentTime.classList.add("past");
+    }
+    else {
+        let currentTime = document.getElementById("15");
+        currentTime.classList.add("future");
+    };
+    if (hour === 16){
         let currentTime = document.getElementById("16");
         currentTime.classList.add("present");
+        timeSlot = 9;
     }
-    else if (hour === 17){
+    else if (timeSlot < hour){
+        let currentTime = document.getElementById("16");
+        currentTime.classList.add("past");
+    }
+    else {
+        let currentTime = document.getElementById("16");
+        currentTime.classList.add("future");
+    };
+    if (hour === 17){
         let currentTime = document.getElementById("17");
         currentTime.classList.add("present");
+        timeSlot = 17;
     }
-    else{
-        console.log("no events")
+    else if (timeSlot < hour){
+        let currentTime = document.getElementById("17");
+        currentTime.classList.add("past");
     }
-
+    else {
+        let currentTime = document.getElementById("17");
+        currentTime.classList.add("future");
+    };
+    
 
 };
 
