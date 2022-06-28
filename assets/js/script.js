d8b1c0db6224250b9c72edde76b345df1c698723
Andrew Baxter <asbaxter1@gmail.com>
2022-06-27 20:46:11 -0400

    class of present added to actual hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,56 @@ function saveTask(){
         var task = $(this).siblings(".description").val();
         let time = $(this).parent().attr("id");
 
-        console.log(time);
-        console.log(task);
-
         localStorage.setItem(time, task);
 
     })
+};
+
+function time(){
+    
+    let hour = moment().hour();
+
+    if (hour === 9){
+        let currentTime = document.getElementById("9");
+        currentTime.classList.add("present");
+    }
+    else if (hour === 10){
+        let currentTime = document.getElementById("10");
+        currentTime.classList.add("present");
+    }
+    else if (hour === 11){
+        let currentTime = document.getElementById("11");
+        currentTime.classList.add("present");
+    }
+    else if (hour === 12){
+        let currentTime = document.getElementById("12");
+        currentTime.classList.add("present");
+    }
+    else if (hour === 13){
+        let currentTime = document.getElementById("13");
+        currentTime.classList.add("present");
+    }
+    else if (hour === 14){
+        let currentTime = document.getElementById("14");
+        currentTime.classList.add("present");
+    }
+    else if (hour === 15){
+        let currentTime = document.getElementById("15");
+        currentTime.classList.add("present");
+    }
+    else if (hour === 16){
+        let currentTime = document.getElementById("16");
+        currentTime.classList.add("present");
+    }
+    else if (hour === 17){
+        let currentTime = document.getElementById("17");
+        currentTime.classList.add("present");
+    }
+    else{
+        console.log("no events")
+    }
+
+
 };
 
 nine = localStorage.getItem("9");
@@ -45,4 +89,5 @@ fifteenText.textContent = fifteen;
 sixteenText.textContent = sixteen;
 seventeenText.textContent = seventeen;
 
+time();
 saveTask();
